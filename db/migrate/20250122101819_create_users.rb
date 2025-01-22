2bd06b069c6aef2ce74e9d53136e610e53968842
伊藤健太 <jiantaiyiteng1@gmail.com>
2025-01-22 19:21:02 +0900

    ユーザーが誕生日を入力するフォームを追加

--- db/migrate/20250122101819_create_users.rb
+++ db/migrate/20250122101819_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.2]
   def change
     create_table :users do |t|
-      t.string :name, null: false
+      t.string :name,     null: false
       t.date :birth_date, null: false
       t.timestamps
     end
