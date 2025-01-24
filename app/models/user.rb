c84b9ceaf2e7cb215006aaa3fbe34c04f26a16c3
伊藤健太 <jiantaiyiteng1@gmail.com>
2025-01-24 16:18:49 +0900

    結果の表示改善

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
     validates :name, :birth_date, presence: true
     validates :name, length: { maximum: 255 }
     def find_closest_national_day
-        birth_month_day = birth_date.strftime('%m-%d')
+        birth_month_day = birth_date.strftime("%m-%d")
         days = NationalDay.all.sort_by do |day|
-          (Date.strptime(day.national_day.strftime('%m-%d'), '%m-%d') - Date.strptime(birth_month_day, '%m-%d')).abs
+          (Date.strptime(day.national_day.strftime("%m-%d"), "%m-%d") - Date.strptime(birth_month_day, "%m-%d")).abs
         end
-    
+
         days.find { |d| (d.national_day - birth_date).abs <= 7 }
     end
 end
