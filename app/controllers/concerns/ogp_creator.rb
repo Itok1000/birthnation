9910a4c494361771d1aa66fa30f7a24cb1cf5253
伊藤健太 <jiantaiyiteng1@gmail.com>
2025-02-03 14:47:17 +0900

    動的OGP修正

--- app/controllers/concerns/ogp_creator.rb
+++ app/controllers/concerns/ogp_creator.rb
@@ -3,7 +3,7 @@ class OgpCreator
     BASE_IMAGE_PATH = "./app/assets/images/ogp.png"  # 実際の画像ファイル名に変更
     GRAVITY = "center"
     TEXT_POSITION = "0,-50" # 国名を少し上に配置
-    TEXT_POSITION_DESCRIPTION = "0,50" # 記念日の日付を少し下に配置
+    TEXT_POSITION_DESCRIPTION = "0,50" # 記念日の説明を少し下に配置
     FONT = "./app/assets/fonts/RocknRollOne-Regular.ttf" # 使用するフォントのパス
     FONT_SIZE = 80
     FONT_SIZE_DESCRIPTION = 30
@@ -21,7 +21,7 @@ class OgpCreator
         config.gravity GRAVITY
         config.pointsize FONT_SIZE
         config.draw "text #{TEXT_POSITION} '#{country_text}'" #　国名
-        config.pointsize FONT_SIZE_DESCRIPTION # 記念日のフォントサイズを変更
+        config.pointsize FONT_SIZE_DESCRIPTION # 記念日の説明フォントサイズを変更
         config.draw "text #{TEXT_POSITION_DESCRIPTION} '#{description_text}'" # 記念日の名前
       end
     end
