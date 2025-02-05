63b3ed3a26b8da39d63b145e9ff17b77dfe5e9a5
伊藤健太 <jiantaiyiteng1@gmail.com>
2025-02-05 10:55:57 +0900

    バグ修正(一時修正でランダム表示を取りやめることにした)

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,11 @@ class User < ApplicationRecord
     birth_month_day = Date.new(2000, birth_month, birth_day) # 基準年を2000年に固定
 
     # ±3日以内の記念日を取得
-    closest_days = NationalDay.all.find do |day|
+    closest_days = NationalDay.all.select do |day|
       national_day_month_day = Date.new(2000, day.national_day.month, day.national_day.day)
       (birth_month_day - national_day_month_day).abs <= 3
     end
+    # 記念日との日数差が最も小さいものを選択
+    closest_days.min_by { |day| (birth_month_day - Date.new(2000, day.national_day.month, day.national_day.day)).abs }
   end
 end
